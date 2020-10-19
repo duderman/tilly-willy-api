8913b0479ffd1fad6870ff919c59609a5e997539
Nikolay Norkin <zduderman@gmail.com>
2020-10-19 21:31:23 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+heart = Product.find_or_initialize_by(code: 1).tap { _1.update!(name: 'Lavender heart', price: 9.25) }
+Product.find_or_initialize_by(code: 2).update!(name: 'Personalised cufflinks', price: 45.00)
+Product.find_or_initialize_by(code: 3).update!(name: 'Kids T-shirt', price: 19.95)
+
+heart_discount_params = { product_id: heart.id, quantity: 2 }
+
+unless Discount::Quantity.where(['configuration @> :params', { params: heart_discount_params.to_json }]).exists?
+  Discount::Quantity.create!(heart_discount_params.merge(price: 8.50))
+end
+
+unless Discount::Total.where(percentage: 10).where(['configuration @> :params', { params: { sum_cents: 6_000 }.to_json }]).exists? # rubocop:disable Layout/LineLength
+  Discount::Total.create!(percentage: 10, sum_cents: 6_000)
+end
